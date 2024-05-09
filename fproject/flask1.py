2e9bb250840238035abbe214a6648c870588f038
Sergeeva Larisa <mindzu@mail.ru>
2024-05-09 17:57:29 +0500

    html

--- fproject/flask1.py
+++ fproject/flask1.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to Flask! Hi all'
+    return "<a href='/courses/1'>Courses</a>" # render_template('my.html')  
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+@app.post('/users')
+def users_post():
+    return 'POST /users'
+
+@app.route('/courses/<id>')
+def courses(id):
+    return f'Course id: {id}'
