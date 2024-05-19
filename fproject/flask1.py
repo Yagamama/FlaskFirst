884d422f3b3dbaf940f370ac3e283733e18656fc
Sergeeva Larisa <mindzu@mail.ru>
2024-05-19 18:29:10 +0500

    users

--- fproject/flask1.py
+++ fproject/flask1.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, get_flashed_messages
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
 
 @app.route('/')
 def hello():
@@ -8,7 +11,14 @@ def hello():
 
 @app.get('/users')
 def users_get():
-    return 'GET /users'
+    term = request.args.get('term', '')
+    if term:
+        filtered = [user for user in users if (term in user)]
+        flash(f'Filtered: "{term}"', 'success')
+    else:
+        filtered = users
+    msg = get_flashed_messages(with_categories=True)
+    return render_template('/users/index.html', users=filtered, search=term, messages=msg)
 
 @app.post('/users')
 def users_post():
